b1e28171f1847cef2b13bea80eedcb2563f9e93f
azizaturrohma <azizaturrohmablt234@gmail.com>
2023-11-22 05:28:17 +0700

    Trial Login API Token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Passport\HasApiTokens;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            "email" => "required",
+            "password" => "required",
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Login gagal, email atau password salah',
+            ]);
+        }
+
+        $token = auth()->user()->createToken('auth_token');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Login berhasil',
+            'access_token' => $token,
+        ]);
+    }
+}
